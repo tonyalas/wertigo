57e331c71ac7f98fbee794eff405af6f3b15ba12
Tony Alas <tony_alas99@hotmail.com>
2021-11-17 18:18:19 -0500

    test changing the server.ts file and changing to express

--- server.ts
+++ server.ts
@@ -1,12 +1,12 @@
-import fast from 'fastify'
-const fastify = fast({ logger: true })
+import express from "express";
 
-fastify.addHook('onRequest', async (request, reply) => {
-    if (request.headers['x-forwarded-proto']) {
-        if (request.headers['x-forwarded-proto'] === 'http') {
-            return reply.redirect(`https://${request.headers.host}${request.raw.url}`)
-        }
-    }
-})
+const app = express()
 
-fastify.listen(port, '0.0.0.0')
+if(process.env.NODE_ENV === 'production') {
+    app.use((req, res, next) => {
+      if (req.header('x-forwarded-proto') !== 'https')
+        res.redirect(`https://${req.header('host')}${req.url}`)
+      else
+        next()
+    })
+  }

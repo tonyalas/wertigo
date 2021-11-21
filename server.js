d45a75d50964b60d84724611635622a52bfc2b80
Tony Alas <tony_alas99@hotmail.com>
2021-11-21 18:20:55 -0500

    tried a new method to send the email contact form

--- server.js
+++ server.js
@@ -18,3 +18,33 @@ app.use(cors({ origin: "*" }));
 //    res.sendFile(process.cwd() + "/index.html");
 //});
 
+app.post('/contactus', (req, res) => {
+    //instantiate the SMTP server
+    const smtpTrans = nodemailer.createTransport({
+        host: 'smtp.gmail.com',
+        port: 465,
+        secure: true,
+        auth: {
+            user: process.env.GMAIL,
+            pass: process.env.PASSWORD,
+        }
+    })
+
+    //specify what the email will look like
+    const mailOpts = {
+        from: 'alastony99@gmail.com',
+        to: process.env.GMAIL,
+        subject: 'New business inquiry email',
+        text: `${req.body.name} (${req.body.email}) says ${req.body.businessName}`
+    }
+
+    // attempt to send the email
+    smtpTrans.sendMail(mailOpts, (error, response) => {
+        if(error) {
+            console.log(error)
+        }
+        else {
+            console.log("success!")
+        }
+    })
+})

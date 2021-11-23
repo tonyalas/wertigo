9246c35a94f043465f004df7b81e1271dfabff1e
Tony Alas <tony_alas99@hotmail.com>
2021-11-22 23:38:39 -0500

    added test carousel to homepage

---
+++ index.js
@@ -0,0 +1,24 @@
+// get the form by its id
+const form = document.getElementById("owner-contact-form");
+
+// create an event listener for when the user submits the form
+const formEvent = form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    // when the event is triggered, create a FormData object called mail based on the input values and their name attributes in the form
+    let mail = new FormData(form);
+
+    // call this function with mail passed as a parameter. This will use Fetch API to post the mail to the url send (from the form element attribute)
+    sendMail(mail);
+});
+
+// defining the sendMail function
+const sendMail = (mail) => {
+    // supply the base url with /send for the fetch(). 
+    fetch("https://www.wertigo.ca/contactus", {
+        method: "post", //  specify the method as post since it is sending data, not getting
+        body: mail,     //  specify the body as mail because we are sending this data in the request
+    }).then((res) => {
+        return res.json();
+    });
+};
--- server.js
+++ server.js
@@ -1,14 +1,14 @@
-import express from "express";
-import cors from "cors";
-import { createTransport } from "nodemailer";
-import multiparty from "multiparty";
-import { urlencoded } from 'body-parser';
+const express = require("express");
+const cors = require("cors");
+const nodemailer = require("nodemailer");
+const multiparty = require("multiparty");
+const bodyParser = require('body-parser')
 require("dotenv").config();
 
 // instantiate an express app
 const app = express()
 
-app.use(urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({extended: true}))
 
 // cors
 app.use(cors({ origin: "*" }));
@@ -20,7 +20,7 @@ app.use(cors({ origin: "*" }));
 
 app.post('/contactus', (req, res) => {
     //instantiate the SMTP server
-    const smtpTrans = createTransport({
+    const smtpTrans = nodemailer.createTransport({
         host: 'smtp.gmail.com',
         port: 465,
         secure: true,

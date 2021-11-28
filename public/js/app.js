059c76231d21bc305d503ecb6b3277f196c31385
Tony Alas <tony_alas99@hotmail.com>
2021-11-28 16:06:05 -0500

    I think the contact form should be working now. Updated server.js and app.js files. Currently using Wertigo email as the recipient email

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,43 @@
 const contactForm = document.querySelector('.owner-contact-form');
 
+let name = document.getElementById('name');
+let email = document.getElementById('email');
+let phoneNumber = document.getElementById('phoneNumber');
+let businessName = document.getElementById('businessName');
+let message = document.getElementById('message');
+
 contactForm.addEventListener('submit', (e) => {
     e.preventDefault();
     console.log('submit clicked')
+
+    let formData = {
+        name: name.value,
+        email: email.value,
+        subject: 'Wertigo Contact Form',
+        phoneNumber: phoneNumber.value,
+        businessName: businessName.value,
+        message: message.value
+    }
+
+    console.log(formData);
+    
+    let xhr = new XMLHttpRequest();
+    xhr.open('POST', '/');
+    xhr.setRequestHeader('content-type', 'application/json');
+    xhr.onload = function() {
+        console.log(xhr.responseText);
+        if(xhr.responseText == 'success') {
+            alert('email sent');
+            name.value = '';
+            email.value = '';
+            phoneNumber.value = '';
+            businessName.value = '';
+            message.value = '';
+        } else {
+            alert('Something went wrong!');
+        }
+    }
+
+    xhr.send(JSON.stringify(formData));
+
 })
--- server.js
+++ server.js
@@ -13,9 +13,39 @@ const PORT = process.env.PORT || 3000;
 // Middleware
 app.use(express.static('public'))
 app.use('/images', express.static('images'));
+app.use(express.json())
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/index.html ')
+})
+
+app.post('/', (req, res) => {
+    console.log(req.body)
+
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: process.env.GMAIL,
+            pass: process.env.PASSWORD
+        }
+    })
+
+    const mailOptions = {
+        from: req.body.email,
+        to: process.env.GMAIL,
+        subject: `Message from ${req.body.email}: ${req.body.subject}`,
+        text: req.body.message
+    }
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if(error) {
+            console.log(error);
+            res.send('error');
+        } else {
+            console.log('Email sent: ' + info.response)
+            res.send('success')
+        }
+    })
 })
 
 

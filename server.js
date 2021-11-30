cea8a0cc8b24129c36030df6ce01e5a824e4ebcd
Tony Alas <tony_alas99@hotmail.com>
2021-11-30 00:06:40 -0500

    test oauth2

--- server.js
+++ server.js
@@ -23,10 +23,10 @@ app.post('/', (req, res) => {
     console.log(req.body)
 
     const transporter = nodemailer.createTransport({
-        host: 'gmail',
+        //host: 'smtp.gmail.com',
         port: 465,
         secure: true,
-        //service: 'gmail',
+        service: 'gmail',
         auth: {
             type: 'OAUTH2',
             user: process.env.GMAIL,

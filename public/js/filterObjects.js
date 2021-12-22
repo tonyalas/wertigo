1ccec7d4b1192341a9eba5ee97db8e3493791acb
Tony Alas <tony_alas99@hotmail.com>
2021-12-22 18:50:49 -0500

    updated show/hide speed in filterObjects.js

--- public/js/filterObjects.js
+++ public/js/filterObjects.js
@@ -6,14 +6,14 @@ $(document).ready(function(){
       if(value == "all")
       {
           //$('.filter').removeClass('hidden');
-          $('.filter').show('400');  // was 1000 before
+          $('.filter').show('1000');  // was 1000 before
       }
       else
       {
 //            $('.filter[filter-item="'+value+'"]').removeClass('hidden');
 //            $(".filter").not('.filter[filter-item="'+value+'"]').addClass('hidden');
-          $(".filter").not('.'+value).hide('400');   // was 3000 before
-          $('.filter').filter('.'+value).show('400');    // was 3000 before
+          $(".filter").not('.'+value).hide('3000');   // was 3000 before
+          $('.filter').filter('.'+value).show('3000');    // was 3000 before
           
       }
   });

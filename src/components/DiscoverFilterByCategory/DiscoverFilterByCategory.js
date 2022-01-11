a4a4db2c8a6698a2fa977b3dd055cb81af56c7eb
Tony Alas <tony_alas99@hotmail.com>
2022-01-11 14:19:13 -0500

    updated more pages to use className instead of class

--- src/components/DiscoverFilterByCategory/DiscoverFilterByCategory.js
+++ src/components/DiscoverFilterByCategory/DiscoverFilterByCategory.js
@@ -8,17 +8,17 @@ function DiscoverFilterByCategory() {
                 <div className='button-section'>
                     <h3 id='filterResults'>Filter Results by Category</h3>
 
-                    <button class='btn btn-secondary category-button filter-button' data-filter='recentlyadded'>Recently Added</button>
-                    <button class='btn btn-secondary category-button filter-button' data-filter='restaurant'>Restaurant</button>
-                    <button class='btn btn-secondary category-button filter-button' data-filter='food'>Food (Other)</button>
-                    <button class='btn btn-secondary category-button filter-button' data-filter='bar'>Bar</button>
-                    <button class='btn btn-secondary category-button filter-button' data-filter='healthandbeauty'>Health & Beauty</button>
-                    <button class='btn btn-secondary category-button filter-button' data-filter='shopping'>Shopping</button>
-                    <button class='btn btn-secondary category-button filter-button' data-filter='accessories'>Accessories</button>
-                    <button class='btn btn-secondary category-button filter-button' data-filter='events'>Events</button>
-                    <button class='btn btn-secondary category-button filter-button' data-filter='entertainment'>Entertainment</button>
-                    <button class='btn btn-secondary category-button filter-button' data-filter='automotive'>Automotive</button>
-                    <button class='btn btn-secondary category-button filter-button' data-filter='contractors'>Contractors</button>
+                    <button className='btn btn-secondary category-button filter-button' data-filter='recentlyadded'>Recently Added</button>
+                    <button className='btn btn-secondary category-button filter-button' data-filter='restaurant'>Restaurant</button>
+                    <button className='btn btn-secondary category-button filter-button' data-filter='food'>Food (Other)</button>
+                    <button className='btn btn-secondary category-button filter-button' data-filter='bar'>Bar</button>
+                    <button className='btn btn-secondary category-button filter-button' data-filter='healthandbeauty'>Health & Beauty</button>
+                    <button className='btn btn-secondary category-button filter-button' data-filter='shopping'>Shopping</button>
+                    <button className='btn btn-secondary category-button filter-button' data-filter='accessories'>Accessories</button>
+                    <button className='btn btn-secondary category-button filter-button' data-filter='events'>Events</button>
+                    <button className='btn btn-secondary category-button filter-button' data-filter='entertainment'>Entertainment</button>
+                    <button className='btn btn-secondary category-button filter-button' data-filter='automotive'>Automotive</button>
+                    <button className='btn btn-secondary category-button filter-button' data-filter='contractors'>Contractors</button>
                 </div>
                 <hr></hr>
             </div>
@@ -28,17 +28,17 @@ function DiscoverFilterByCategory() {
                     <Accordion.Item eventKey='0'>
                         <Accordion.Header><h4 className='subHeader'>Filter Results by Subcategory</h4></Accordion.Header>
                         <Accordion.Body>
-                            <p class='filter recentlyadded'>Please select a category from above to see subcategory options.</p>
-                            <p class='filter restaurant italian mexican chinese fastfood none-button'>Restaurant</p>
-                            <p class='filter food sauces cafe deli grocerystore none-button'>Food (Other)</p>
-                            <p class='filter bar sportsbar casual highend danceclub none-button'>Bar</p>
-                            <p class='filter healthandbeauty massagetherapy barbershop hairsalon nails eyebrows eyelashes none-button'>Health & Beauty</p>
-                            <p class='filter shopping art clothing shoes usedgoods none-button'>Shopping</p>
-                            <p class='filter accessories gifts jewelry none-button'>Accessories</p>
-                            <p class='filter events weddings none-button'>Events</p>
-                            <p class='filter entertainment none-button'>Entertainment</p>
-                            <p class='filter automotive carrepairs oilchanges caraccessories none-button'>Automotive</p>
-                            <p class='filter contractors none-button'>Contractors</p>
+                            <p className='filter recentlyadded'>Please select a category from above to see subcategory options.</p>
+                            <p className='filter restaurant italian mexican chinese fastfood none-button'>Restaurant</p>
+                            <p className='filter food sauces cafe deli grocerystore none-button'>Food (Other)</p>
+                            <p className='filter bar sportsbar casual highend danceclub none-button'>Bar</p>
+                            <p className='filter healthandbeauty massagetherapy barbershop hairsalon nails eyebrows eyelashes none-button'>Health & Beauty</p>
+                            <p className='filter shopping art clothing shoes usedgoods none-button'>Shopping</p>
+                            <p className='filter accessories gifts jewelry none-button'>Accessories</p>
+                            <p className='filter events weddings none-button'>Events</p>
+                            <p className='filter entertainment none-button'>Entertainment</p>
+                            <p className='filter automotive carrepairs oilchanges caraccessories none-button'>Automotive</p>
+                            <p className='filter contractors none-button'>Contractors</p>
                         </Accordion.Body>
                     </Accordion.Item>
                 </Accordion>
